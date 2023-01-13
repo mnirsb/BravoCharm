5d975235e3c7eaa363bd4269410830600a811223
mnirsb <mnirsb@gmail.com>
2023-01-13 18:54:48 +0530

    intial setup

---
+++ src/main/java/com/bravocharm/HurricaneL1Application.java
@@ -0,0 +1,13 @@
+package com.bravocharm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HurricaneL1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HurricaneL1Application.class, args);
+	}
+
+}
